055edd8f58a56f600bb8653bf35d9605e26488ab
Asanobm <asanobm@outlook.com>
2024-03-02 06:55:06 +0900

    Add Google Analytics tracking code to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9PD9TH787D"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-9PD9TH787D');
+        </script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
