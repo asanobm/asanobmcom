c2daefe7e1f396821876d28c802fd96ad6b93ad0
Asanobm <asanobm@outlook.com>
2024-03-02 05:40:13 +0900

    add: src, tailwind

--- constants.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @Summary: This file contains all the constants used in the application.
- */
-import path from "path";
-
-export const BASE_PATH = '/posts'
-export const POSTS_PATH = path.join(process.cwd(), BASE_PATH)
