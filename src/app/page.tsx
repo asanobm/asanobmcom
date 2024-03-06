296d052ac85845c68f0e771d8133962ed0822dad
Asanobm <asanobm@outlook.com>
2024-03-07 08:42:25 +0900

    Refactor post listing and add comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@ import { getAllPosts } from "@/lib/post";
 import Image from "next/image";
 
 export default function Home() {
-  const posts = getAllPosts('**');
+  const allPosts = getAllPosts('**');
+  // Last 5 posts sorted by date
+  const last5Posts = allPosts
+    .sort((a, b) => new Date(b.frontmatter.date).getTime() - new Date(a.frontmatter.date).getTime())
+    .slice(0, 5);
+  
   return (
     <div>
       <h1>Posts</h1>
+      {/* Last 5 posts  */}
       <ul>
-        {posts.map((post) => {
+        {allPosts.map((post) => {
           return (
             <li key={post.slug}>
               <Image src={post.frontmatter.thumbnail} alt="thumbnail" width={200} height={200} />
@@ -18,6 +24,8 @@ export default function Home() {
           )
         })}
       </ul>
+
+      {/* Blog Card */}
     </div>
   )
 }
