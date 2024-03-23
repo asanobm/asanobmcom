682b3319e3d7bb246b234c8b1bdbee7a7f9d58df
Asanobm <asanobm@outlook.com>
2024-03-23 15:41:45 +0900

    Refactor page components and update imports

--- src/app/[lang]/[slug]/page.tsx
+++ src/app/[lang]/[slug]/page.tsx
@@ -1,14 +1,14 @@
-import MDXContent from "@/components/MdxContent";
 import { getPost } from "@/lib/post";
+import { MdxContent } from "@/comp";
 
-async function Page({ params }: { params: { slug: string } }) {
-  const {serialized, frontmatter} = await getPost('ko', params.slug);
+async function Page({ params }: { params: { lang: string, slug: string } }) {
+  const {serialized, frontmatter} = await getPost(params.lang, params.slug);
   return (
     <div>
       <h1>{frontmatter.title}</h1>
       <p>{frontmatter.date}</p>
       <div className={'markdown-body'}>
-        <MDXContent source={serialized} />
+        <MdxContent source={serialized} />
       </div>
     </div>
   );
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,12 +1,12 @@
 import { getAllPosts } from '@/lib/post'
-import { getDictionary } from './dictionaries'
+import { getDictionary } from '@/app/[lang]/dictionaries'
  
 export default async function Page({ params }: { params: { lang: string } }) {
-  const dict = await getDictionary(params.lang)
   const posts = getAllPosts(params.lang)
+  const dict = await getDictionary(params.lang)
   return (
     <div>
-      <h1>Posts</h1>
+      <h1 className='text-2xl font-bold'>{dict.products.cart}</h1>
       <ul>
         {posts.map((post) => {
           return (
